732b582c1303b05901440ca806e2c0bb7953768a
Thiago Novato <thiagonovato@gmail.com>
2022-01-28 14:57:45 -0300

    Implementando contextapi na autenticação.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,15 +1,19 @@
 import React, { createContext, useEffect, useState } from "react";
 import auth from "@react-native-firebase/auth";
+import { Alert } from "react-native";
 
 interface AuthContextData {
   signed: boolean;
   user: object | null;
   signOut(): void;
+  signIn(email: string, password: string): Promise<void>;
+  loadingAuth: boolean;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [loadingAuth, setLoadingAuth] = useState<boolean>(false);
   const [user, setUser] = useState<object | null>(null);
 
   useEffect(() => {
@@ -22,8 +26,22 @@ export const AuthProvider: React.FC = ({ children }) => {
     auth().signOut();
   }
 
+  async function signIn(email: string, password: string) {
+    setLoadingAuth(true);
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        Alert.alert("Erro ao logar", "Verifique seus dados e tente novamente.");
+      })
+      .finally(() => {
+        setLoadingAuth(false);
+      });
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signOut }}>
+    <AuthContext.Provider
+      value={{ signed: !!user, user, signOut, signIn, loadingAuth }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,28 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
-import auth from "@react-native-firebase/auth";
 import { Container, Account, Title, Subtitle } from "./styles";
 import { ButtonText } from "../../components/ButtonText";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
-import { Alert } from "react-native";
+import AuthContext from "../../contexts/AuthContext";
 
 export function SignIn() {
+  const { signIn, loadingAuth } = useContext(AuthContext);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState<boolean>(false);
-
-  function handleSignInWithEmailAndPassword() {
-    setLoading(true);
-    auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(() => {
-        Alert.alert("Erro ao logar", "Verifique seus dados e tente novamente.");
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }
 
   return (
     <Container>
@@ -39,9 +27,9 @@ export function SignIn() {
 
       <Button
         title="Entrar"
-        onPress={handleSignInWithEmailAndPassword}
+        onPress={() => signIn(email, password)}
         disabled={!email || !password}
-        loading={loading}
+        loading={loadingAuth}
       />
 
       {/* <Account>
